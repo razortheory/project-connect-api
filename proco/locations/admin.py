7d4c58b75668c5ea998a6ea2cfc6b11860e0777f
Roman Karpovich <fpm.th13f@gmail.com>
2020-08-19 14:05:22 +0300

    use mapbox for country geometry

--- proco/locations/admin.py
+++ proco/locations/admin.py
@@ -1,32 +1,33 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from mapbox_location_field.spatial.admin import SpatialMapAdmin
 
 from proco.locations.models import Country, Location
 from proco.utils.admin import CountryNameDisplayAdminMixin
 
 
 @admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
-    list_display = ('flag_preview', 'name', 'code', 'location_preview')
+class CountryAdmin(SpatialMapAdmin):
+    list_display = ('name', 'code', 'flag_preview', 'location_preview')
     search_fields = ('name',)
-    list_display_links = ('flag_preview', 'name', 'location_preview')
+    exclude = ('geometry_simplified',)
 
     def flag_preview(self, obj):
         if not obj.flag:
             return ''
-        return f'<img src="{obj.flag.url}" style="max-width:50px; max-height:50px;" />'
-
+        return mark_safe(f'<img src="{obj.flag.url}" style="max-width:50px; max-height:50px;" />')
     flag_preview.short_description = 'Flag'
 
     def location_preview(self, obj):
         if not obj.map_preview:
             return ''
-        return f'<img src="{obj.map_preview.url}" style="max-width:100px; max-height:100px;" />'
-
+        return mark_safe(f'<img src="{obj.map_preview.url}" style="max-width:100px; max-height:100px;" />')
     location_preview.short_description = 'Map Preview'
 
 
 @admin.register(Location)
-class LocationAdmin(CountryNameDisplayAdminMixin, admin.ModelAdmin):
+class LocationAdmin(CountryNameDisplayAdminMixin, SpatialMapAdmin):
     list_display = ('name', 'get_country_name')
     list_select_related = ('country', 'parent')
     search_fields = ('name', 'country__name')
+    exclude = ('geometry_simplified',)
