db2f0b868d085636bb6ce992700487bb21a3bc96
Nikita.G <nikita.g@razortheory.com>
2020-08-21 14:34:26 +0300

    Cleaned code

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,9 @@ from django.urls import include, path
 from django.utils.translation import ugettext as _
 
 
-admin.site.site_header = _("Project Connect")
-admin.site.site_title = _("Project Connect")
-admin.site.index_title = _("Welcome to Project Connect")
+admin.site.site_header = _('Project Connect')
+admin.site.site_title = _('Project Connect')
+admin.site.index_title = _('Welcome to Project Connect')
 
 
 urlpatterns = [
