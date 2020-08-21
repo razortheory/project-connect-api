3198c0ef0eb22105cafaa911589d30244314aaf1
Nikita.G <nikita.g@razortheory.com>
2020-08-21 14:44:38 +0300

    Sorted imports

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import ugettext as _
 
-
 admin.site.site_header = _('Project Connect')
 admin.site.site_title = _('Project Connect')
 admin.site.index_title = _('Welcome to Project Connect')
