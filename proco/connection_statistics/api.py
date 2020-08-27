bc9f2c88a611836ef11170358cb82f1d9c473ae4
Nikita.G <nikita.g@razortheory.com>
2020-08-27 11:29:47 +0300

    Fixed imports order

--- proco/connection_statistics/api.py
+++ proco/connection_statistics/api.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
