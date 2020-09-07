041c9e505a2a0c55d4c3cd26838c2d379e0a02b4
Roman Karpovich <fpm.th13f@gmail.com>
2020-09-07 14:39:55 +0300

    fixed missing / for countries-boundary

--- proco/locations/api_urls.py
+++ proco/locations/api_urls.py
@@ -11,6 +11,6 @@ router.register(r'countries', api.CountryViewSet, basename='countries')
 app_name = 'locations'
 
 urlpatterns = [
-    path('countries-boundary', api.CountryBoundaryListAPIView.as_view(), name='countries-boundary'),
+    path('countries-boundary/', api.CountryBoundaryListAPIView.as_view(), name='countries-boundary'),
     path('', include(router.urls)),
 ]
