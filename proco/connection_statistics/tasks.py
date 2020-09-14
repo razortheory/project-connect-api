ff7f6ff1756c98d31aa433a105615f2e99a0bce7
Nikita.G <nikita.g@razortheory.com>
2020-09-14 10:13:38 +0300

    Added Brasil statistic loader

--- proco/connection_statistics/tasks.py
+++ proco/connection_statistics/tasks.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
 from celery.schedules import crontab
 from celery.task import periodic_task
 
@@ -7,6 +11,7 @@ from proco.connection_statistics.utils import (
     aggregate_real_time_data_to_school_daily_status,
     aggregate_school_daily_status_to_school_weekly_status,
 )
+from proco.schools.loaders.brasil_loader import brasil_statistic_loader
 
 
 @periodic_task(run_every=crontab(hour='*/6'))
@@ -19,3 +24,9 @@ def aggregate_real_time_data():
 def aggregate_daily_statistics():
     aggregate_school_daily_status_to_school_weekly_status()
     aggregate_country_daily_status_to_country_weekly_status()
+
+
+@periodic_task(run_every=crontab(hour=0, minute=0))
+def load_brasil_daily_statistics():
+    brasil_statistic_loader.update_schools()
+    brasil_statistic_loader.update_statistic(timezone.now().date() - timedelta(days=1))
