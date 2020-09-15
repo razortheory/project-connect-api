1f46a0eebb761fb884f4ab1a10bd3435e5a20c7f
Roman Karpovich <fpm.th13f@gmail.com>
2020-09-15 17:49:08 +0300

    cache country details

--- proco/locations/api.py
+++ proco/locations/api.py
@@ -47,6 +47,10 @@ class CountryViewSet(
         return serializer_class
 
     @method_decorator(cache_page(timeout=settings.CACHES['default']['TIMEOUT']))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @method_decorator(cache_page(timeout=settings.CACHES['default']['TIMEOUT']))
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
