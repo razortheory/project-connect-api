703caafb03e57d48bd23abe7d4f7b9ec47f83484
Roman Karpovich <fpm.th13f@gmail.com>
2020-09-21 10:42:59 +0300

    fixed celery periodic tasks key

--- proco/taskapp/__init__.py
+++ proco/taskapp/__init__.py
@@ -14,14 +14,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.beat_schedule = {
-    'update_real_time_data': {
-        'task': 'proco.connection_statistics.update_real_time_data',
+    'proco.connection_statistics.tasks.update_real_time_data': {
+        'task': 'proco.connection_statistics.tasks.update_real_time_data',
         # todo: we need to make more flexible system to update data for previous day
         'schedule': crontab(hour='6,12,18,22', minute=0),
         'args': (),
     },
-    'update_brasil_schools': {
-        'task': 'proco.connection_statistics.update_brasil_schools',
+    'proco.connection_statistics.tasks.update_brasil_schools': {
+        'task': 'proco.connection_statistics.tasks.update_brasil_schools',
         'schedule': crontab(hour=1, minute=0),
         'args': (),
     },
