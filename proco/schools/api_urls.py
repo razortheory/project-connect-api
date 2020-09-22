0d68c17d8b832f74d78af3f4ddd87726a320c981
Andrey <andrey.f@razortheory.com>
2020-09-22 15:58:46 +0300

    csv export api

--- proco/schools/api_urls.py
+++ proco/schools/api_urls.py
@@ -7,10 +7,13 @@ from proco.schools import api
 
 country_schools = routers.NestedSimpleRouter(location_router, r'countries', lookup='country')
 country_schools.register(r'schools', api.SchoolsViewSet, basename='schools')
+country_schools.register(r'export-csv-schools', api.CSVExportSchoolsListViewSet, basename='export_csv_schools')
+
 
 app_name = 'schools'
 
 urlpatterns = [
     path('', include(country_schools.urls)),
     path('schools/random/', api.RandomSchoolsListAPIView.as_view(), name='random-schools'),
+    # path('export-csv-schools/', api.CSVExportSchoolsListView.as_view(), name='export-schools-csv')
 ]
