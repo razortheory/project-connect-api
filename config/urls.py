8cbcb85616d55e64fa3b03a436265d5774553613
Andrey <andrey.f@razortheory.com>
2020-09-24 17:21:40 +0300

    added cache clear button to admin dashboard

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.utils.translation import ugettext as _
-
-admin.site.site_header = _('Project Connect')
-admin.site.site_title = _('Project Connect')
-admin.site.index_title = _('Welcome to Project Connect')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
