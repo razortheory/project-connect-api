ab01e1cabf03c592ae9a79c40038fc55354a3eec
Nikita.G <nikita.g@razortheory.com>
2020-10-05 14:25:18 +0300

    Get rid of timeput on location admin

--- proco/locations/admin.py
+++ proco/locations/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from django_mptt_admin.admin import DjangoMpttAdmin
-
 from proco.locations.models import Country, Location
+from proco.locations.filters import CountryFilterList
 from proco.utils.admin import CountryNameDisplayAdminMixin
 
 
@@ -22,10 +21,13 @@ class CountryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Location)
-class LocationAdmin(CountryNameDisplayAdminMixin, DjangoMpttAdmin):
-    tree_auto_open = False
+class LocationAdmin(CountryNameDisplayAdminMixin, admin.ModelAdmin):
     list_display = ('name', 'get_country_name')
-    list_select_related = ('country', 'parent')
+    list_filter = (CountryFilterList,)
     search_fields = ('name', 'country__name')
     exclude = ('geometry_simplified',)
     raw_id_fields = ('parent', 'country')
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(LocationAdmin, self).get_queryset(*args, **kwargs).defer('geometry', 'geometry_simplified')
+        return qs
