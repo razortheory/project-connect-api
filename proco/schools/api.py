bed119a3d9cad8dbd24190e2d4dfa741754d30d7
Nikita.G <nikita.g@razortheory.com>
2020-10-21 15:51:33 +0300

    Select weekly status

--- proco/schools/api.py
+++ proco/schools/api.py
@@ -24,7 +24,7 @@ class SchoolsViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
-    queryset = School.objects.all()
+    queryset = School.objects.all().select_related('last_weekly_status')
     pagination_class = None
     serializer_class = SchoolSerializer
     filter_backends = (
