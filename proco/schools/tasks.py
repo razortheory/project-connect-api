ba0d0f39910ca9d7e9ae9d4bb06e50447f30d223
Nikita.G <nikita.g@razortheory.com>
2020-10-30 10:47:19 +0300

    Connect fileImport with country

--- proco/schools/tasks.py
+++ proco/schools/tasks.py
@@ -1,19 +1,50 @@
 import traceback
+from collections import Counter
 
+from django.contrib.gis.geos import MultiPoint, Point
 from django.core.cache import cache
 from django.db import transaction
-from django.utils import timezone
 
-from proco.connection_statistics.models import SchoolWeeklyStatus
 from proco.connection_statistics.utils import update_specific_country_weekly_status
 from proco.locations.models import Country
 from proco.schools.loaders import ingest
+from proco.schools.loaders.ingest import load_data
 from proco.schools.models import FileImport
 from proco.taskapp import app
 
 
 class FailedImportError(Exception):
     pass
+
+
+def _find_country(loaded) -> Country:
+    points = MultiPoint()
+    for _i, data in enumerate(loaded):
+        try:
+            point = Point(x=float(data['lon']), y=float(data['lat']))
+
+            if point == Point(x=0, y=0):
+                continue
+            else:
+                points.append(point)
+        except (TypeError, ValueError):
+            continue
+
+    countries = list(Country.objects.filter(geometry__intersects=points))
+
+    if not countries:
+        return None
+    elif len(countries) == 1:
+        return countries[0]
+    else:
+        countries_counter = Counter()
+        for country in countries:
+            instersections = country.geometry.intersection(points)
+            if isinstance(instersections, Point):
+                countries_counter[country] = 1
+            else:
+                countries_counter[country] = len(instersections)
+        return countries_counter.most_common()[0][0]
 
 
 @app.task(soft_time_limit=30 * 60, time_limit=30 * 60)
@@ -24,13 +55,19 @@ def process_loaded_file(pk: int, force: bool = False):
 
     imported_file.status = FileImport.STATUSES.started
     imported_file.save()
-    begin_time = timezone.now()
 
     try:
+        imported_file.country = _find_country(load_data(imported_file.uploaded_file))
+        if not imported_file.country:
+            imported_file.status = FileImport.STATUSES.failed
+            imported_file.errors = 'Error: Country not found'
+            imported_file.save()
+            return
+
         # todo: rewrite with transaction savepoint
         try:
             with transaction.atomic():
-                warnings, errors = ingest.save_data(imported_file.uploaded_file)
+                warnings, errors = ingest.save_data(imported_file.country, load_data(imported_file.uploaded_file))
                 if errors and not force:
                     raise FailedImportError
         except FailedImportError:
@@ -52,12 +89,7 @@ def process_loaded_file(pk: int, force: bool = False):
 
         if not errors or force:
             def update_stats():
-                countries = SchoolWeeklyStatus.objects.filter(
-                    created__gt=begin_time,
-                ).order_by('school__country_id').values_list('school__country_id').distinct()
-                countries = Country.objects.filter(id__in=countries)
-                for country in countries:
-                    update_specific_country_weekly_status(country)
+                update_specific_country_weekly_status(imported_file.country)
                 cache.clear()
 
             transaction.on_commit(update_stats)
