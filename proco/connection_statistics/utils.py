03f80c1fb5e82860b8c05f7aa76ebf49aa8a0301
Roman Karpovich <fpm.th13f@gmail.com>
2020-11-20 17:53:25 +0300

    updated country status aggregation. added coverage pie chart

--- proco/connection_statistics/utils.py
+++ proco/connection_statistics/utils.py
@@ -4,6 +4,14 @@ from datetime import timedelta
 from django.db.models import Avg, Count, Q
 from django.utils import timezone
 
+from proco.connection_statistics.aggregations import (
+    aggregate_connectivity_by_availability,
+    aggregate_connectivity_by_speed,
+    aggregate_connectivity_default,
+    aggregate_coverage_by_availability,
+    aggregate_coverage_by_types,
+    aggregate_coverage_default,
+)
 from proco.connection_statistics.models import (
     CountryDailyStatus,
     CountryWeeklyStatus,
@@ -12,7 +20,7 @@ from proco.connection_statistics.models import (
     SchoolWeeklyStatus,
 )
 from proco.locations.models import Country
-from proco.schools.constants import statuses_schema
+from proco.schools.constants import ColorMapSchema
 from proco.schools.models import School
 from proco.utils.dates import get_current_week, get_current_year
 
@@ -110,80 +118,82 @@ def update_country_weekly_status(country: Country):
         country.last_weekly_status = country_status
         country.save()
 
-    country_status.schools_total = country.schools.count()
+    # calculate pie charts. first we need to understand which case is applicable for country
+    latest_statuses = SchoolWeeklyStatus.objects.filter(school__country=country, _school__isnull=False)
+    connectivity_types = CountryWeeklyStatus.CONNECTIVITY_TYPES_AVAILABILITY
+    coverage_types = CountryWeeklyStatus.COVERAGE_TYPES_AVAILABILITY
 
-    schools_stats = SchoolWeeklyStatus.objects.filter(
-        year=country_status.year, week=country_status.week, school__country=country,
-    ).aggregate(
-        connectivity_no=Count(
-            'connectivity', filter=Q(connectivity_speed=0),
-        ),
-        connectivity_unknown=Count(
-            'connectivity', filter=Q(connectivity_speed__isnull=True),
-        ),
-        connectivity_moderate=Count(
-            'connectivity', filter=Q(
-                connectivity_speed__gte=statuses_schema.CONNECTIVITY_SPEED_FOR_GOOD_CONNECTIVITY_STATUS,
-            ),
-        ),
-        connectivity_good=Count(
-            'connectivity', filter=Q(
-                connectivity_speed__gt=0,
-                connectivity_speed__lt=statuses_schema.CONNECTIVITY_SPEED_FOR_GOOD_CONNECTIVITY_STATUS,
-            ),
-        ),
-        connectivity_available=Count(
-            'connectivity', filter=Q(connectivity__in=[True, False]),
-        ),
-        coverage_availability=Count(
-            'connectivity', filter=Q(coverage_availability__in=[True, False]),
-        ),
-        coverage_type=Count(
-            'connectivity', filter=Q(
-                coverage_type__in=[type_[0] for type_ in SchoolWeeklyStatus.COVERAGE_TYPES if type_[0] != 'unknown']),
-        ),
+    if RealTimeConnectivity.objects.filter(school__country=country).exists():
+        country_status.connectivity_availability = connectivity_types.realtime_speed
+        connectivity_stats = aggregate_connectivity_by_speed(latest_statuses)
+    elif SchoolWeeklyStatus.objects.filter(school__country=country, connectivity_speed__gt=0).exists():
+        country_status.connectivity_availability = connectivity_types.static_speed
+        connectivity_stats = aggregate_connectivity_by_speed(latest_statuses)
+    elif SchoolWeeklyStatus.objects.filter(school__country=country, connectivity__isnull=False).exists():
+        country_status.connectivity_availability = connectivity_types.connectivity
+        connectivity_stats = aggregate_connectivity_by_availability(latest_statuses)
+    else:
+        country_status.connectivity_availability = connectivity_types.no_connectivity
+        connectivity_stats = aggregate_connectivity_default(latest_statuses)
+
+    if SchoolWeeklyStatus.objects.filter(school__country=country)\
+            .exclude(coverage_type=SchoolWeeklyStatus.COVERAGE_TYPES.unknown).exists():
+        country_status.coverage_availability = coverage_types.coverage_type
+        coverage_stats = aggregate_coverage_by_types(latest_statuses)
+    elif SchoolWeeklyStatus.objects.filter(school__country=country, coverage_availability__isnull=False).exists():
+        country_status.coverage_availability = coverage_types.coverage_availability
+        coverage_stats = aggregate_coverage_by_availability(latest_statuses)
+    else:
+        country_status.coverage_availability = coverage_types.no_coverage
+        coverage_stats = aggregate_coverage_default(latest_statuses)
+
+    # remember connectivity pie chart
+    country_status.schools_connectivity_good = connectivity_stats[ColorMapSchema.GOOD]
+    country_status.schools_connectivity_moderate = connectivity_stats[ColorMapSchema.MODERATE]
+    country_status.schools_connectivity_no = connectivity_stats[ColorMapSchema.NO]
+    country_status.schools_connectivity_unknown = connectivity_stats[ColorMapSchema.UNKNOWN]
+
+    # remember coverage pie chart
+    country_status.schools_coverage_good = coverage_stats[ColorMapSchema.GOOD]
+    country_status.schools_coverage_moderate = coverage_stats[ColorMapSchema.MODERATE]
+    country_status.schools_coverage_no = coverage_stats[ColorMapSchema.NO]
+    country_status.schools_coverage_unknown = coverage_stats[ColorMapSchema.UNKNOWN]
+
+    # calculate speed & latency where available
+    schools_stats = latest_statuses.aggregate(
+        total=Count('*'),
         connectivity_speed=Avg('connectivity_speed', filter=Q(connectivity_speed__gt=0)),
         connectivity_latency=Avg('connectivity_latency', filter=Q(connectivity_latency__gt=0)),
     )
 
-    if schools_stats['connectivity_available']:
-        country_status.connectivity_availability = country_status.CONNECTIVITY_TYPES_AVAILABILITY.connectivity
-    else:
-        country_status.connectivity_availability = country_status.CONNECTIVITY_TYPES_AVAILABILITY.no_connectivity
-    if schools_stats['connectivity_moderate'] or schools_stats['connectivity_good'] or schools_stats['connectivity_no']:
-        country_status.connectivity_availability = country_status.CONNECTIVITY_TYPES_AVAILABILITY.static_speed
-        if country.daily_status.exists():
-            country_status.connectivity_availability = country_status.CONNECTIVITY_TYPES_AVAILABILITY.realtime_speed
-
-    if schools_stats['coverage_availability']:
-        country_status.coverage_availability = CountryWeeklyStatus.COVERAGE_TYPES_AVAILABILITY.coverage_availability
-    else:
-        country_status.coverage_availability = CountryWeeklyStatus.COVERAGE_TYPES_AVAILABILITY.no_coverage
-    if schools_stats['coverage_type']:
-        country_status.coverage_availability = CountryWeeklyStatus.COVERAGE_TYPES_AVAILABILITY.coverage_type
-
     country_status.connectivity_speed = schools_stats['connectivity_speed']
     country_status.connectivity_latency = schools_stats['connectivity_latency']
 
-    overall_connected_schools = SchoolWeeklyStatus.objects.filter(
-        school__country=country, connectivity=True,
-    ).order_by('school_id').distinct('school_id').count()
-
-    country_status.schools_connectivity_unknown = schools_stats['connectivity_unknown']
-    country_status.schools_connectivity_no = schools_stats['connectivity_no']
-    country_status.schools_connectivity_moderate = schools_stats['connectivity_moderate']
-    country_status.schools_connectivity_good = schools_stats['connectivity_good']
-    country_status.schools_connected = schools_stats['connectivity_moderate'] + schools_stats['connectivity_good']
+    country_status.schools_total = schools_stats['total']
+    schools_with_data = country_status.schools_connectivity_moderate + country_status.schools_connectivity_good
+    country_status.schools_connected = schools_with_data
     if country_status.schools_total:
-        country_status.schools_with_data_percentage = 1.0 * overall_connected_schools / country_status.schools_total
+        country_status.schools_with_data_percentage = 1.0 * country_status.schools_connected
+        country_status.schools_with_data_percentage /= country_status.schools_total
     else:
         country_status.schools_with_data_percentage = 0
 
+    # move country status as far as we can
     if country_status.integration_status == CountryWeeklyStatus.JOINED and country_status.schools_total:
         country_status.integration_status = CountryWeeklyStatus.SCHOOL_MAPPED
-    if country_status.integration_status == CountryWeeklyStatus.SCHOOL_MAPPED and country_status.connectivity_speed:
+
+    if country_status.integration_status == CountryWeeklyStatus.SCHOOL_MAPPED and any([
+        country_status.schools_connectivity_good,
+        country_status.schools_connectivity_moderate,
+        country_status.schools_connectivity_no,
+        country_status.schools_coverage_good,
+        country_status.schools_coverage_moderate,
+        country_status.schools_coverage_no,
+    ]):
         country_status.integration_status = CountryWeeklyStatus.STATIC_MAPPED
-    if country_status.integration_status == CountryWeeklyStatus.STATIC_MAPPED and country.daily_status.exists():
+
+    if country_status.integration_status == CountryWeeklyStatus.STATIC_MAPPED \
+            and country_status.connectivity_availability == connectivity_types.realtime_speed:
         country_status.integration_status = CountryWeeklyStatus.REALTIME_MAPPED
 
     country_status.avg_distance_school = country.calculate_avg_distance_school()
