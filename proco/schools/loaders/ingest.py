47263714d3673ed212b0d7de4db8dd2b21a0544b
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-02 15:13:59 +0300

    disabled schools matching by name; fixed integrity error on matching school to multiple statistics entries

--- proco/schools/loaders/ingest.py
+++ proco/schools/loaders/ingest.py
@@ -10,7 +10,7 @@ from proco.schools.loaders.pipeline import (  # remove_too_close_points,
     get_validated_rows,
     map_schools_by_external_id,
     map_schools_by_geopoint,
-    map_schools_by_name,
+    remove_mapped_twice_schools,
     update_existing_schools,
     update_schools_weekly_statuses,
 )
@@ -35,14 +35,15 @@ def load_data(uploaded_file):
     return loader.load_file(uploaded_file)
 
 
-def save_data(country: Country, loaded: Iterable[dict], ignore_errors=False) -> Tuple[List[str], List[str]]:
+def save_data(country: Country, loaded: Iterable[dict], ignore_errors=False) -> Tuple[List[str], List[str], int]:
     schools_data, errors, warnings = get_validated_rows(country, loaded)
     if errors and not ignore_errors:
         return warnings, errors, 0
 
     map_schools_by_external_id(country, schools_data)
-    map_schools_by_name(country, schools_data)
     map_schools_by_geopoint(country, schools_data)
+    schools_data, new_warnings = remove_mapped_twice_schools(schools_data)
+    warnings.extend(new_warnings)
 
     # new_errors = remove_too_close_points(country, schools_data)
     # errors.extend(new_errors)
