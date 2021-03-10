3e08f756607fad7b3ec0ebee293f2e437698e40e
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-10 12:51:50 +0300

    set host for flask

--- celery.sh
+++ celery.sh
@@ -4,6 +4,6 @@ set -ex
 # run flask app to
 pipenv run pip install flask
 export FLASK_APP=hello.py
-pipenv run python -m flask run --port 8000 &
+pipenv run python -m flask run --host 0.0.0.0 --port 8000 &
 
 pipenv run celery -A proco.taskapp worker -l INFO
