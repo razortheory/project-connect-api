8772374578c462bdff1a733795814eb01cfb8a7a
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-10 11:58:15 +0300

    added flask helloworld to run celery worker in azure appservice

---
+++ celery.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ex
+
+# run flask app to
+pipenv run pip install flask
+FLASK_APP=hello.py pipenv run python -m flask run --port 8000 &
+
+pipenv run celery -A proco.taskapp worker -l INFO
