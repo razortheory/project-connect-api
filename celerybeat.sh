4045256145c8bf06ee385c04db8de433e94f5282
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-11 17:05:37 +0300

    celerybeat startup script

---
+++ celerybeat.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ex
+
+# run flask app to
+pipenv run pip install flask
+export FLASK_APP=hello.py
+pipenv run python -m flask run --host 0.0.0.0 --port 8000 &
+
+pipenv run celery -A proco.taskapp beat -l INFO
