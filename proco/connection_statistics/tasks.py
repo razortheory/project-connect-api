456dff01870e9b0ab19e2270bb992c17d2d4a01c
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-18 12:55:18 +0300

    increased time limit for aggregate country data task

--- proco/connection_statistics/tasks.py
+++ proco/connection_statistics/tasks.py
@@ -11,7 +11,7 @@ from proco.schools.loaders.brasil_loader import brasil_statistic_loader
 from proco.taskapp import app
 
 
-@app.task(soft_time_limit=60 * 60, time_limit=60 * 60)
+@app.task(soft_time_limit=4 * 60 * 60, time_limit=4 * 60 * 60)
 def aggregate_country_data(_prev_result, country_id, *args):
     country = Country.objects.get(id=country_id)
     aggregate_real_time_data_to_school_daily_status(country)
