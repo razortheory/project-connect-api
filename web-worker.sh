6f5f2cbf8e0918a63307af8bc94d37fa0961a6e5
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-22 12:25:29 +0300

    added ssh config to all startup files

--- web-worker.sh
+++ web-worker.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+echo "Starting SSH ..."
+service ssh start
+
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic --noinput
 pipenv run gunicorn config.wsgi:application -b 0.0.0.0:8000 -w 8 --timeout=60
