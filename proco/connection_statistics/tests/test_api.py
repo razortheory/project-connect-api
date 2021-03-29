6aa3579271b3df7b190029a0fc08fa573254d99f
Roman Karpovich <fpm.th13f@gmail.com>
2021-03-29 14:34:56 +0300

    added test for yesterday data

--- proco/connection_statistics/tests/test_api.py
+++ proco/connection_statistics/tests/test_api.py
@@ -1,42 +1,20 @@
-import json
-import os
 import random
-from datetime import datetime, timedelta
-from unittest.mock import patch
+from datetime import datetime
 
 from django.core.cache import cache
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
 
 from rest_framework import status
 
-from pytz import UTC
-
-from proco.connection_statistics.models import (
-    CountryDailyStatus,
-    CountryWeeklyStatus,
-    RealTimeConnectivity,
-    SchoolDailyStatus,
-    SchoolWeeklyStatus,
-)
+from proco.connection_statistics.models import CountryWeeklyStatus
 from proco.connection_statistics.tests.factories import (
     CountryDailyStatusFactory,
     CountryWeeklyStatusFactory,
-    RealTimeConnectivityFactory,
-    SchoolDailyStatusFactory,
     SchoolWeeklyStatusFactory,
 )
-from proco.connection_statistics.utils import (
-    aggregate_real_time_data_to_school_daily_status,
-    aggregate_school_daily_status_to_school_weekly_status,
-    aggregate_school_daily_to_country_daily,
-    update_country_weekly_status,
-)
 from proco.locations.tests.factories import CountryFactory
-from proco.schools.loaders.brasil_loader import brasil_statistic_loader
 from proco.schools.tests.factories import SchoolFactory
-from proco.utils.dates import get_current_week, get_current_year
 from proco.utils.tests import TestAPIViewSetMixin
 
 
@@ -166,140 +144,3 @@ class CountryDailyStatsApiTestCase(TestAPIViewSetMixin, TestCase):
                     'country_id': self.country_one.id,
                 }),
             )
-
-
-class AggregateConnectivityDataTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.country = CountryFactory()
-        cls.school = SchoolFactory(country=cls.country)
-        RealTimeConnectivityFactory(school=cls.school, connectivity_speed=4000000)
-        RealTimeConnectivityFactory(school=cls.school, connectivity_speed=6000000)
-
-    def test_aggregate_real_time_data_to_school_daily_status(self):
-        aggregate_real_time_data_to_school_daily_status(self.country, timezone.now().date())
-        self.assertEqual(SchoolDailyStatus.objects.count(approx=False), 1)
-        self.assertEqual(SchoolDailyStatus.objects.first().connectivity_speed, 5000000)
-
-    def test_aggregate_real_time_data_to_country_daily_status(self):
-        aggregate_real_time_data_to_school_daily_status(self.country, timezone.now().date())
-        aggregate_school_daily_to_country_daily(self.country, timezone.now().date())
-        self.assertEqual(CountryDailyStatus.objects.count(), 1)
-        self.assertEqual(CountryDailyStatus.objects.first().connectivity_speed, 5000000)
-
-    def test_aggregate_school_daily_to_country_daily(self):
-        today = datetime.now().date()
-        SchoolDailyStatusFactory(school__country=self.country, connectivity_speed=4000000, date=today)
-        SchoolDailyStatusFactory(school__country=self.country, connectivity_speed=6000000, date=today)
-
-        aggregate_school_daily_to_country_daily(self.country, timezone.now().date())
-        self.assertEqual(CountryDailyStatus.objects.get(country=self.country, date=today).connectivity_speed, 5000000)
-
-    def test_aggregate_country_daily_status_to_country_weekly_status(self):
-        CountryDailyStatusFactory(country=self.country, date=datetime.now().date())
-        SchoolWeeklyStatusFactory(
-            school__country=self.country,
-            connectivity=True, connectivity_speed=4000000,
-            coverage_availability=True, coverage_type='unknown',
-            year=get_current_year(), week=get_current_week(),
-        )
-        SchoolWeeklyStatusFactory(
-            school__country=self.country, connectivity_speed=6000000, year=get_current_year(), week=get_current_week(),
-        )
-
-        country_weekly = CountryWeeklyStatus.objects.filter(country=self.country).last()
-        if not country_weekly.is_verified:
-            country_weekly.update_country_status_to_joined()
-
-        update_country_weekly_status(self.country)
-        self.assertEqual(CountryWeeklyStatus.objects.filter(country=self.country).count(), 1)
-        self.assertEqual(CountryWeeklyStatus.objects.filter(country=self.country).last().connectivity_speed, 5000000)
-
-        country_weekly = CountryWeeklyStatus.objects.filter(country=self.country).last()
-        self.assertEqual(country_weekly.integration_status, CountryWeeklyStatus.REALTIME_MAPPED)
-        self.assertEqual(country_weekly.connectivity_availability,
-                         CountryWeeklyStatus.CONNECTIVITY_TYPES_AVAILABILITY.realtime_speed)
-        self.assertEqual(country_weekly.coverage_availability,
-                         CountryWeeklyStatus.COVERAGE_TYPES_AVAILABILITY.coverage_availability)
-
-    def test_aggregate_school_daily_status_to_school_weekly_status(self):
-        today = datetime.now().date()
-        SchoolDailyStatusFactory(school=self.school, connectivity_speed=4000000, date=today - timedelta(days=1))
-        SchoolDailyStatusFactory(school=self.school, connectivity_speed=6000000, date=today)
-
-        self.school.last_weekly_status = None
-        self.school.save()
-        aggregate_school_daily_status_to_school_weekly_status(self.country)
-        self.school.refresh_from_db()
-        self.assertNotEqual(self.school.last_weekly_status, None)
-        self.assertEqual(SchoolWeeklyStatus.objects.count(), 1)
-        self.assertEqual(SchoolWeeklyStatus.objects.last().connectivity_speed, 5000000)
-        self.assertEqual(SchoolWeeklyStatus.objects.last().connectivity, True)
-
-    def test_aggregate_school_daily_status_to_school_weekly_status_connectivity_unknown(self):
-        # daily status is too old, so it wouldn't be involved into country calculations
-        today = datetime.now().date()
-        SchoolDailyStatusFactory(school=self.school, connectivity_speed=None, date=today - timedelta(days=8))
-        SchoolWeeklyStatusFactory(
-            school=self.school, week=get_current_week(), year=get_current_year(), connectivity=None,
-        )
-
-        aggregate_school_daily_status_to_school_weekly_status(self.country)
-        self.assertEqual(SchoolWeeklyStatus.objects.count(), 1)
-        self.assertEqual(SchoolWeeklyStatus.objects.last().connectivity, None)
-
-    def test_aggregate_school_daily_status_to_school_weekly_status_connectivity_no(self):
-        today = datetime.now().date()
-        SchoolDailyStatusFactory(school=self.school, connectivity_speed=None, date=today - timedelta(days=8))
-        SchoolWeeklyStatusFactory(
-            school=self.school, week=get_current_week(), year=get_current_year(), connectivity=False,
-        )
-
-        aggregate_school_daily_status_to_school_weekly_status(self.country)
-        self.assertEqual(SchoolWeeklyStatus.objects.count(), 1)
-        self.assertEqual(SchoolWeeklyStatus.objects.last().connectivity, False)
-
-
-class TestBrazilParser(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        with open(os.path.join('proco', 'connection_statistics', 'tests', 'data', 'brazil_example.json')) as example:
-            cls.example_data = json.load(example)
-        cls.country = CountryFactory(code='BR')
-
-    @patch('proco.schools.loaders.brasil_loader.BrasilSimnetLoader.load_schools_statistic')
-    def test_unknown_schools(self, load_mock):
-        load_mock.return_value = self.example_data
-
-        today = datetime.now().date()
-        brasil_statistic_loader.update_statistic(today)
-        self.assertEqual(RealTimeConnectivity.objects.filter(school__country=self.country).count(), 0)
-
-    @patch('proco.schools.loaders.brasil_loader.BrasilSimnetLoader.load_schools_statistic')
-    def test_data_loaded(self, load_mock):
-        load_mock.return_value = self.example_data
-        SchoolFactory(country=self.country, external_id=43029094)
-        SchoolFactory(country=self.country, external_id=41062310)
-        today = datetime.now().date()
-
-        with self.assertNumQueries(6):
-            brasil_statistic_loader.update_statistic(today)
-
-        # records shouldn't be saved during second call, so there are less queries expected
-        with self.assertNumQueries(4):
-            brasil_statistic_loader.update_statistic(today)
-
-        self.assertEqual(RealTimeConnectivity.objects.filter(school__country=self.country).count(), 3)
-        self.assertListEqual(
-            list(
-                RealTimeConnectivity.objects.filter(
-                    school__country=self.country,
-                ).order_by('created').values_list('created', flat=True),
-            ),
-            [
-                datetime(year=2020, month=9, day=15, hour=1, minute=10, second=37, tzinfo=UTC),
-                datetime(year=2020, month=9, day=15, hour=5, minute=16, second=36, tzinfo=UTC),
-                datetime(year=2020, month=9, day=15, hour=7, minute=16, second=36, tzinfo=UTC),
-            ],
-        )
